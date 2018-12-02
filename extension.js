509742c0b7269aeab0897f77f17bc08197dbb4d8
mallowlabs <mallowlabs@gmail.com>
2018-12-02 10:38:09 +0900

    Changed: rename command name

--- extension.js
+++ extension.js
@@ -34,7 +34,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', async function() {
+    let disposable = vscode.commands.registerCommand('extension.toggleSpec', async function() {
         // The code you place here will be executed every time your command is executed
 
         let editor = vscode.window.activeTextEditor;
